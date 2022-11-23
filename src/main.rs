f2b119223ba4d115e106b6ec5da8680182a3505a
Eshan Singh <eshansingh@gmail.com>
2022-11-23 10:24:21 -0500

    Add logging infrastructure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    #[cfg(debug_assertions)]
+    app.add_plugins(DefaultPlugins.set(LogPlugin {
+        filter: "warn,bevy_portal=debug".into(),
+        level: Level::DEBUG,
+    }));
+
+    #[cfg(not(debug_assertions))]
+    app.add_plugins(DefaultPlugins.set(LogPlugin {
+        filter: "warn".into(),
+        level: Level::WARN,
+    }));
+
+    app.run();
 }
